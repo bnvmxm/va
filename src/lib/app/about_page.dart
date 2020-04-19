9114fe51581e678ab676aef5d955e572cac635a5
Maxim Biyanov <maxim_biyanov@epam.com>
2020-04-19 17:04:24 +0300

    Add License

--- src/lib/app/about_page.dart
+++ src/lib/app/about_page.dart
@@ -58,7 +58,7 @@ class AboutPage extends StatelessWidget {
                         child: Text("""
 \u00A9
 
-Copyright 2020 https://github.com/kugjo
+Copyright 2020 Maxim Biyanov
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
